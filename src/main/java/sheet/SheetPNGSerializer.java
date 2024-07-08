3def33ff657338092aed06f07acf47dc2fd16df7
nguyenvoankhoa <nguyenvoankhoa@gmail.com>
2024-07-08 16:37:42 +0700

    complete test case

--- src/main/java/sheet/SheetPNGSerializer.java
+++ src/main/java/sheet/SheetPNGSerializer.java
@@ -3,12 +3,8 @@ package sheet;
 import dependency.ISheetSerialize;
 import file.IOMessage;
 import file.IOStatus;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 
 public class SheetPNGSerializer implements ISheetSerialize {
-    private static final Log LOGGER = LogFactory.getLog(SheetPNGSerializer.class);
-
     @Override
     public IOMessage saveMindMap(Sheet sheet, String filepath) {
         return new IOMessage(IOStatus.SUCCESS, "Save to PNG success");
--- src/main/java/sheet/SheetWordSerializer.java
+++ src/main/java/sheet/SheetWordSerializer.java
@@ -3,12 +3,7 @@ package sheet;
 import dependency.ISheetSerialize;
 import file.IOMessage;
 import file.IOStatus;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 public class SheetWordSerializer implements ISheetSerialize {
-    private static final Log LOGGER = LogFactory.getLog(SheetPNGSerializer.class);
-
     @Override
     public IOMessage saveMindMap(Sheet sheet, String filepath) {
         return new IOMessage(IOStatus.SUCCESS, "Save to Word success");
